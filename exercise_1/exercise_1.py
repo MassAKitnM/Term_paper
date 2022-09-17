f851c0ba56174dd8d05e359aec6633903c20188b
MassAKitnM <f.sidorchuk@gmail.com>
2022-09-17 22:09:07 +0300

    exersise 1

---
+++ exercise_1/exercise_1.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+if __name__ == '__main__':
+
+    with open("resourse_1.txt", "r", encoding="utf-8") as fin:
+        text = fin.read()
+        words = text.split()
+        words_dict = defaultdict(int)
+        for el in words:
+            words_dict[el] += 1
+
+        words_sorted = sorted(words_dict.items(), key=lambda x: (-x[1], x[0]))
+
+        with open("result_1.txt", "w", encoding="utf-8") as fout:
+            for word, count in words_sorted:
+                print(word, count, file=fout)
