48af3754dc11cd4674b9fbff515973db2350bbd1
MassAKitnM <f.sidorchuk@gmail.com>
2022-09-18 01:11:46 +0300

    ex.2 UPDATE (now true end)

--- exercise_2/exercise_2.py
+++ exercise_2/exercise_2.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from collections import defaultdict
+from tkinter import scrolledtext
 
 if __name__ == "__main__":
 
@@ -27,44 +28,55 @@ if __name__ == "__main__":
                 if self.clients[name] > 0:
                     self.clients[name] = int(self.clients[name] * (1 + p/100))
 
-    def clicked():
-        s = ent.get()
-        text_el = s.split()
-        lbl.configure(text=s)
-        if text_el[0] == "DEPOSIT":
-            bank.deposit(text_el[1], int(text_el[2]))
-            lbl.configure(text="COMMAND ACCEPT, INPUT NEXT COMAND")
-        elif text_el[0] == "WITHDRAW":
-            bank.withdraw(text_el[1], int(text_el[2]))
-            lbl.configure(text="COMMAND ACCEPT, INPUT NEXT COMAND")
-        elif text_el[0] == "BALANCE":
-            if len(text_el) > 1:
-                tmp = bank.balance(text_el[1])
-            else:
-                tmp = bank.balance("")
-                tmp2 = ""
-                for key, value in tmp:
-                    tmp2 += key + ' ' + str(value) + '\n'
-            lbl.configure(text=tmp2)
-        elif text_el[0] == "TRANSFER":
-            bank.transfer(text_el[1], text_el[2], int(text_el[3]))
-            lbl.configure(text="COMMAND ACCEPT, INPUT NEXT COMAND")
-        elif text_el[0] == "INCOME":
-            bank.income(int(text_el[1]))
-            lbl.configure(text="COMMAND ACCEPT, INPUT NEXT COMAND")
-        else:
-            lbl.configure(text=text_el[0])
+    def clicked_calculate():
+        s = str(text.get(1.0, END))
+        text_el = s.split('\n')
+        lbl_text = ""
+        for el in text_el:
+            tmp = el.split(' ')
+            if tmp[0] == "DEPOSIT":
+                bank.deposit(tmp[1], int(tmp[2]))
+            elif tmp[0] == "WITHDRAW":
+                bank.withdraw(tmp[1], int(tmp[2]))
+            elif tmp[0] == "BALANCE":
+                if len(tmp) > 1:
+                    out = bank.balance(tmp[1])
+                    lbl_text += tmp[1] + ' ' + str(out) + '\n'
+                else:
+                    data = bank.balance("")
+                    out = ""
+                    for key, value in data:
+                        out += key + ' ' + str(value) + '\n'
+                    lbl_text += out
+            elif tmp[0] == "TRANSFER":
+                bank.transfer(tmp[1], tmp[2], int(tmp[3]))
+            elif tmp[0] == "INCOME":
+                bank.income(int(tmp[1]))
+        lbl.configure(text=lbl_text)
+
+    def clicked_clear():
+        text.delete(1.0, END)
+        lbl.configure(text="need some input")
 
     bank = Bank()
     bank.deposit("Sidorchuk", 61899)
 
     window = Tk()
     window.title("Bank")
-    window.geometry('600x250')
-    lbl = Label(window, text="need some input")
-    ent = Entry(window, width=100)
-    ent.grid(column=0, row=0)
-    lbl.grid(column=0, row=3)
-    btn = Button(window, text="Calculate", command=clicked)
-    btn.grid(column=0, row=1)
+    window.geometry('600x600')
+    lbl_input = Label(window, text="Input:")
+    lbl_input.place(x=120, y=0)
+    lbl_output = Label(window, text="Output:")
+    lbl_output.place(x=433, y=0)
+    lbl = Label(window, text="need some input",
+                width=39, height=34, bg="#406080", foreground="white", borderwidth=3, relief="solid", anchor=NW, justify="left")
+    lbl.place(x=310, y=20)
+    text = scrolledtext.ScrolledText(window, width=34, height=32, bg="#406080",
+                                     foreground="white", borderwidth=3, relief="solid")
+    text.place(x=10, y=20)
+    btn_calculate = Button(window, text="Calculate",
+                           command=clicked_calculate)
+    btn_calculate.place(x=310, y=560)
+    btn_clear = Button(window, text="reset text", command=clicked_clear)
+    btn_clear.place(x=450, y=560)
     window.mainloop()
